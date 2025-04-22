d1fd769c590ed6499973b01bbec794bbdd56a7af
teb0so <joaopedrostcunha@protonmail.com>
2025-04-22 20:58:58 -0300

    Desafio: Nível Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,25 +6,44 @@ int main() {
   int tabuleiro[10][10] = {0};
 
   //Inicializar posição do navio horizontal
-  int linha_inicial_1 = 5;
-  int coluna_inicial_1 = 5;
-  int tamanho_1 = 3;
+  int linha_inicial_h = 5;
+  int coluna_inicial_h = 5;
+  int tamanho_h = 3;
 
   //posiciona navio horizontal no tabuleiro
-  for (int i = 0; i < tamanho_1; i++) {
-    tabuleiro[linha_inicial_1 + i][coluna_inicial_1] = 3;
+  for (int i = 0; i < tamanho_h; i++) {
+    tabuleiro[linha_inicial_h + i][coluna_inicial_h] = 3;
   }
 
   //Inicializar posição do navio vertical
-  int linha_inicial_2 = 1;
-  int coluna_inicial_2 = 1;
-  int tamanho_2 = 3;
-
+  int linha_inicial_v = 1;
+  int coluna_inicial_v = 1;
+  int tamanho_v = 3;
 
   //posiciona navio vertical no tabuleiro
-  for (int i = 0; i < tamanho_2; i++) {
-    tabuleiro[linha_inicial_2][coluna_inicial_2 + i] = 3;
+  for (int i = 0; i < tamanho_v; i++) {
+    tabuleiro[linha_inicial_v][coluna_inicial_v + i] = 3;
   }
+
+  //Inicializar posição do navio diagonal 1
+  int linha_inicial_d1 = 1;
+  int coluna_inicial_d1 = 5;
+  int tamanho_d1 = 3;
+
+  //posiciona navio diagonal 1
+    for (int i = 0; i < tamanho_d1; i++){
+      tabuleiro[linha_inicial_d1 + i][coluna_inicial_d1 + i] = 3;
+    }
+
+  //Inicializar posição do navio diagonal 2
+  int linha_inicial_d2 = 6;
+  int coluna_inicial_d2 = 1;
+  int tamanho_d2 = 3;
+
+  //posiciona navio diagonal 1
+    for (int i = 0; i < tamanho_d2; i++){
+      tabuleiro[linha_inicial_d2 - i][coluna_inicial_d2 + i] = 3;
+    }
 
   //Imprimir tabuleiro
   printf("     A B C D E F G H I J\n    ____________________\n");
